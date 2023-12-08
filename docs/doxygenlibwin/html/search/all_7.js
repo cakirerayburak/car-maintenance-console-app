71c58541097be3b4ffe0a08ed4ecd21885f7e82d
cakirerayburak <cakirerayburak@gmail.com>
2023-12-09 01:46:44 +0300

    CmakeList updated again and .bat files renamed for project

---
+++ docs/doxygenlibwin/html/search/all_7.js
@@ -0,0 +1,15 @@
+var searchData=
+[
+  ['calculatemean_0',['calculateMean',['../class_coruh_1_1_utility_1_1_math_utility.html#abe3d63ed2737ae3c006bcc5732a3d01f',1,'Coruh::Utility::MathUtility']]],
+  ['calculatemedian_1',['calculateMedian',['../class_coruh_1_1_utility_1_1_math_utility.html#aa5888ea1bef95e477a66c2dd8517b885',1,'Coruh::Utility::MathUtility']]],
+  ['calculateminmax_2',['calculateMinMax',['../class_coruh_1_1_utility_1_1_math_utility.html#ac66a656b821b690761a1db545359508d',1,'Coruh::Utility::MathUtility']]],
+  ['calculator_20project_20library_20generation_20and_20testing_20template_3',['Calculator Project Library Generation and Testing Template',['../index.html',1,'']]],
+  ['can_20effect_20on_20wsl_4',['can effect on wsl',['../index.html#autotoc_md4',1,'Step-1 (Run on Windows, Can Effect on WSL)'],['../index.html#autotoc_md5',1,'Step-2 (Run on Windows, Can Effect on WSL)']]],
+  ['clean_20project_5',['Clean Project',['../index.html#autotoc_md12',1,'']]],
+  ['commontypes_2eh_6',['commonTypes.h',['../common_types_8h.html',1,'']]],
+  ['comparedouble_7',['compareDouble',['../class_coruh_1_1_utility_1_1_math_utility.html#a8a30d3bb9945b043c861257596f17c30',1,'Coruh::Utility::MathUtility']]],
+  ['converter_8',['Install Test Results to HTML Converter',['../index.html#autotoc_md16',1,'']]],
+  ['coruh_9',['Coruh',['../namespace_coruh.html',1,'']]],
+  ['coruh_3a_3autility_10',['Utility',['../namespace_coruh_1_1_utility.html',1,'Coruh']]],
+  ['coverage_20ratios_11',['coverage ratios',['../index.html#autotoc_md15',1,'Documentation Coverage Ratios'],['../index.html#autotoc_md14',1,'Test Coverage Ratios']]]
+];
